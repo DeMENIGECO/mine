280dbcaddde5592326aded5a4199a8a5846a9587
DomeniGeco <domenigeco@gmail.com>
2026-07-03 19:04:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const fs = require("fs");
 
+const app = express();
+
+app.use(express.json());
+app.use(express.static("admin"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/admin/index.html");
+});
+
+app.listen(3000, () => {
+    console.log("Mine avviato su http://localhost:3000");
+});
